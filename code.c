b1f31d980e6e73a1efe1d75077f7bbb12557d6b3
devilking <devilkingsiva15@gmail.com>
2026-03-05 18:18:09 +0530

    first commit

---
+++ code.c
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car { // The class
+  public: // Access specifier
+    string brand;
+    string model;
+    int year;
+};
+
+int main() {
+  Car carObj1; // Create an object of Car
+  carObj1.brand = "BMW";
+  carObj1.model = "X5";
+  carObj1.year = 1999;
+
+  Car carObj2; // Create another object of Car
+  carObj2.brand = "Ford";
+  carObj2.model = "Mustang";
+  carObj2.year = 1969;
+
+  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << endl;
+  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << endl;
+
+  return 0;
+}
